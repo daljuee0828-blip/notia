a5692e3d91338d2420648170eaf5426353fdf4bd
daljuee0828-blip <daljuee0828@gmail.com>
2026-08-04 14:13:59 +0900

    lib/widgets/tool_button.dart 만들기

---
+++ lib/widgets/tool_button.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class ToolButton extends StatelessWidget {
+
+  final IconData icon;
+  final String title;
+
+  const ToolButton({
+    super.key,
+    required this.icon,
+    required this.title,
+  });
+
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Card(
+      margin: const EdgeInsets.only(bottom: 15),
+
+      child: ListTile(
+        leading: Icon(icon),
+        title: Text(
+          title,
+          style: const TextStyle(
+            fontSize: 18,
+          ),
+        ),
+
+        trailing: const Icon(
+          Icons.arrow_forward_ios,
+        ),
+      ),
+    );
+  }
+}
