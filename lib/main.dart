f7c00d69fc041f6ac972593a8ac6ef1f640776db
daljuee0828-blip <daljuee0828@gmail.com>
2026-08-04 14:13:20 +0900

    lib/main.dart 만들기

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'screens/home_screen.dart';
+
+void main() {
+  runApp(const NotiaApp());
+}
+
+class NotiaApp extends StatelessWidget {
+  const NotiaApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Notia',
+      theme: ThemeData(
+        useMaterial3: true,
+        colorSchemeSeed: Colors.blue,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
