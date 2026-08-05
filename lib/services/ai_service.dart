6ede4e0966237750a4d91b022b6374f28720b4db
daljuee0828-blip <daljuee0828@gmail.com>
2026-08-05 11:03:06 +0900

    ai_service.dart 업데이트

--- lib/services/ai_service.dart
+++ lib/services/ai_service.dart
@@ -1,18 +1,207 @@
-class AIService {
+import 'package:flutter/material.dart';
+import 'services/ai_service.dart';
 
-  Future<String> askAI(String question) async {
 
-    // 지금은 테스트 답변
-    // 다음 단계에서 여기에 Groq API 연결
+class AIScreen extends StatefulWidget {
 
-    await Future.delayed(
-      const Duration(seconds: 1),
-    );
+  const AIScreen({super.key});
 
-    return 
-    "AI 답변 준비 완료!\n\n"
-    "질문: $question";
+
+  @override
+  State<AIScreen> createState() => _AIScreenState();
+
+}
+
+
+
+class _AIScreenState extends State<AIScreen> {
+
+
+  final TextEditingController controller =
+      TextEditingController();
+
+
+  String answer =
+      "안녕하세요! Notia AI 선생님입니다 🤖";
+
+
+
+  Future<void> askAI() async {
+
+
+    if(controller.text.isEmpty){
+
+      setState(() {
+
+        answer = "질문을 입력해주세요!";
+
+      });
+
+      return;
+
+    }
+
+
+
+    setState(() {
+
+      answer = "AI 생각 중... 🤖";
+
+    });
+
+
+
+    final result =
+        await AIService().askAI(
+          controller.text
+        );
+
+
+
+    setState(() {
+
+      answer = result;
+
+    });
+
 
   }
+
+
+
+  @override
+  Widget build(BuildContext context) {
+
+
+    return Scaffold(
+
+
+      appBar: AppBar(
+
+        title: const Text("AI 선생님"),
+
+      ),
+
+
+
+      body: Padding(
+
+
+        padding: const EdgeInsets.all(20),
+
+
+        child: Column(
+
+
+          children: [
+
+
+
+            Expanded(
+
+
+              child: Card(
+
+
+                child: Padding(
+
+
+                  padding: const EdgeInsets.all(16),
+
+
+                  child: SingleChildScrollView(
+
+
+                    child: Text(
+
+                      answer,
+
+                      style: const TextStyle(
+
+                        fontSize:18,
+
+                      ),
+
+                    ),
+
+                  ),
+
+                ),
+
+              ),
+
+            ),
+
+
+
+
+            const SizedBox(height:15),
+
+
+
+
+            TextField(
+
+
+              controller: controller,
+
+
+              decoration: const InputDecoration(
+
+                hintText:"궁금한 내용을 입력하세요",
+
+                border:OutlineInputBorder(),
+
+              ),
+
+
+            ),
+
+
+
+
+            const SizedBox(height:10),
+
+
+
+
+            SizedBox(
+
+
+              width:double.infinity,
+
+
+              child: ElevatedButton(
+
+
+                onPressed:askAI,
+
+
+                child: const Text(
+
+                  "AI에게 질문하기"
+
+                ),
+
+
+              ),
+
+            )
+
+
+          ],
+
+
+        ),
+
+
+      ),
+
+
+    );
+
+
+  }
+
 
 }
